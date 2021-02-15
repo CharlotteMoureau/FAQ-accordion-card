d632de09d280ca736cde5ec465a91b90e4e18253
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-02-15 19:09:25 +0100

    js

---
+++ src/accordion.js
@@ -0,0 +1,19 @@
+// const accordion = document.getElementsByClassName("faq-points");
+
+// for (let i = 0; i < accordion.length; i++) {
+//     accordion[i].addEventListener("click", function () {
+//         /* Toggle between adding and removing the "active" class,
+//         to highlight the button that controls the panel */
+//         this.classList.toggle("active");
+
+//         /* Toggle between hiding and showing the active panel */
+//         let panel = this.nextElementSibling;
+//         console.log(panel);
+//         if (panel.style.display === "block") {
+//             panel.style.display = "none";
+//         } else {
+//             panel.style.display = "block";
+//         }
+//         console.log(panel);
+//     });
+// }

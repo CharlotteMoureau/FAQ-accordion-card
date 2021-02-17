1e8ef8e5a8ae07459581dfce23f1fe7ca4660fe7
CharlotteMoureau <Charlotte.Moureau96@gmail.com>
2021-02-17 14:00:06 +0100

    fini accordéon

--- src/accordion.js
+++ src/accordion.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const accordion = document.getElementsByClassName('question');
-    console.log(accordion);
 
     for (let i = 0; i < accordion.length; i++) {
         accordion[i].addEventListener("click", function () {
+
             let panel = this.nextElementSibling;
-            console.log(i);
+            console.log(panel);
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
                 panel.style.display = "block";
             }
+
         });
     }
 });
